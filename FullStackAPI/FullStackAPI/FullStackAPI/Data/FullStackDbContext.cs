a515137453ce554cbb2a1b0668decaabf2b83694
Klleriston <Klleandrade@outlook.com>
2024-01-03 20:30:50 -0300

    first commit

---
+++ FullStackAPI/FullStackAPI/FullStackAPI/Data/FullStackDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FullStackAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStackAPI.Data
+{
+    public class FullStackDbContext : DbContext
+    {
+        public FullStackDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ FullStackAPI/FullStackAPI/FullStackAPI/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace FullStackAPI.Models
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public long Phone { get; set; }
+        public long Salary { get; set; }
+        public string Department { get; set; }
+    }
+}
